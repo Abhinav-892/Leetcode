b6707952dd4bebc156aeef185d04a392d6344df3
Abhinav-892 <abhinavd681@gmail.com>
2026-06-26 01:32:04 +0530

    Time: 7 ms (13.97%) | Memory: 11.7 MB (74.66%) - LeetSync

---
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+Node*solve(Node*head){
+    if(!head){
+        return NULL ;
+    }
+    auto it = head ;
+    auto tail = it->next ;
+
+   while(it){
+    if(it->child){
+        auto childTail = solve(it->child) ;
+        auto temp = it->next ;
+        it->next = it->child ;
+        it->next->prev = it ;
+        childTail->next = temp ;
+        if(temp){
+            temp->prev = childTail ;
+        }
+       it->child = NULL ;
+    }
+    tail = it ;
+    it = it->next ;
+   }
+   return tail ;
+}
+ 
+
+    Node* flatten(Node* head) {
+        solve(head) ;
+        return head ;   
+    }
+};
