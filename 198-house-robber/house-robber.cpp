bef70f2b22b42e15d0968f1857a447a43ae926fa
Abhinav-892 <abhinavd681@gmail.com>
2026-07-09 00:46:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (78.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -34,6 +34,56 @@ public:
   }
 
 
+//Method-2 Tabulation/Bottom-Up Approach
+
+int solveUsingTabulation(vector<int>& nums){
+
+//Step1: dp array create intialise
+   int n = nums.size() ;
+   vector<int> dp(n+1,-1) ; 
+
+ //Step2: analyse base case and fill dp array
+  dp[n] = 0 ;
+
+//Step3: reverse the flow,loop,copy paste
+ for(int i=n-1 ; i>=0 ; i--){
+    int temp = 0 ;
+    if(i+2<n){
+        temp = dp[i+2] ;
+    }
+      int include = nums[i] + temp ;
+      int exclude = 0 + dp[i+1] ;
+      dp[i] = max(include,exclude) ;
+ }
+ //step4: return ans
+   return dp[0] ;
+} 
+
+
+
+//Method-3 SpaceOptimisation
+int solveUsingSpaceOptimisation(vector<int>& nums){
+    int n = nums.size() ;
+
+    int next1 = 0 ;
+    int next2 = 0 ;
+    int curr ;
+ 
+ for(int i=n-1 ; i>=0 ; i--){
+    int temp = 0 ;
+    if(i+2<n){
+        temp = next2 ;
+    }
+      int include = nums[i] + temp ;
+      int exclude = 0 + next1 ;
+      curr = max(include,exclude) ;
+
+      next2 = next1 ;
+      next1 = curr ;
+ }
+  return curr ;
+}
+
 
 
 
@@ -43,9 +93,16 @@ public:
     //    int ans = solveUsingRecursion(nums,index) ; 
       
     //Method1 - Top Down
-    int n = nums.size() ;
-    vector<int> dp(n+1,-1) ; 
-    int ans = solveUsingMem(nums,index,dp) ; 
+    // int n = nums.size() ;
+    // vector<int> dp(n+1,-1) ; 
+    // int ans = solveUsingMem(nums,index,dp) ; 
+
+
+    //Method-2 Tabulation/Bottom-Up Approach
+    // int ans = solveUsingTabulation(nums) ;
+
+    //Method-3 SpaceOptimisation
+    int ans = solveUsingSpaceOptimisation(nums) ;
       
        return ans ;
     }
