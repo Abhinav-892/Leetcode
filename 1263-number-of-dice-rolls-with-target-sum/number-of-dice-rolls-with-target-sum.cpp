159d5a7bba8b6de42ae56af9234584d8fc236e1e
Abhinav-892 <abhinavd681@gmail.com>
2026-07-12 01:21:41 +0530

    Time: 35 ms (5.36%) | Memory: 9.5 MB (84.59%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -55,15 +55,72 @@ int solveUsingMem(int n,int k,int target,vector<vector<long long int>>& dp){
 }
 
 
+//Method2 tabulation
 
- 
+ int solveUsingTabulation(int n,int k,int target){
+    vector<vector<long long int>> dp(n+1,vector<long long int>(target+1,0));
+    dp[0][0] = 1 ;
+
+  for(int a=1 ; a<=n; a++){
+    for(int t=1 ; t<=target; t++){
+        long long int ans = 0 ;
+        for(int val=1; val<=k; val++){
+            long long int temp = 0 ;
+            if(t-val>=0){
+                temp = dp[a-1][t-val]%mod ;
+            }
+        ans = (ans%mod + temp)%mod ;
+    }
+      dp[a][t] = ans ;
+    }
+  }
+     return dp[n][target] ;
+ }
+
+
+//Method3 Space_Optimisation
+
+int solveUsingOS(int n,int k,int target){
+    vector<int> prev(target+1,0) ;
+    vector<int> curr(target+1,0) ;
+
+    prev[0] = 1 ;
+
+    for(int a=1 ; a<=n; a++){
+    for(int t=1 ; t<=target; t++){
+        long long int ans = 0 ;
+        for(int val=1; val<=k; val++){
+            long long int temp = 0 ;
+            if(t-val>=0){
+                temp = prev[t-val]%mod ;
+            }
+        ans = (ans%mod + temp)%mod ;
+    }
+      curr[t] = ans ;
+    }
+    //bhuljata hu
+    //Shifting
+   prev = curr ;
+}
+ return prev[target] ;
+}
+
+
     int numRollsToTarget(int n, int k, int target) {
     //    int ans = solveUsingRec(n,k,target) ;
     //    return ans ;       
 
     //Method1 
-    vector<vector<long long int>> dp(n+1,vector<long long int>(target+1,-1));
-    int ans = solveUsingMem(n,k,target,dp) ;
-      return ans ;
+    // vector<vector<long long int>> dp(n+1,vector<long long int>(target+1,-1));
+    // int ans = solveUsingMem(n,k,target,dp) ;
+    //   return ans ;
+    
+    //Method2
+    // int ans = solveUsingTabulation(n,k,target) ;  
+    // return ans ;
+
+    //Method3
+    int ans = solveUsingOS(n,k,target) ;
+    return ans ;
     }
 };
